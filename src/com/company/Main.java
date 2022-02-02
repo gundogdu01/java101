80f15b8cd2eaa015f82ea0baad11bf4c442b7d2b
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-02 21:38:46 +0300

    kdv oran hesaplama

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double tutar,kdvOran = 0.18,kdvTutar, kdvliTutar;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir tutar giriniz: ");
+        tutar = input.nextDouble();
+
+        if (tutar>0 && tutar<1000) {
+            kdvTutar = kdvOran * tutar;
+            kdvliTutar = kdvTutar + tutar;
+
+            System.out.println("Kdv'siz tutar: " + tutar);
+            System.out.println("Kdv oran: " + kdvOran);
+            System.out.println("Kdv tutarı: " + kdvTutar);
+            System.out.println("Kdvli tutar: " + kdvliTutar);
+        }else if(tutar>=1000){
+            kdvOran = 0.08;
+            kdvTutar = kdvOran * tutar;
+            kdvliTutar = kdvTutar + tutar;
+
+            System.out.println("Kdv'siz tutar: " + tutar);
+            System.out.println("Kdv oran: " + kdvOran);
+            System.out.println("Kdv tutarı: " + kdvTutar);
+            System.out.println("Kdvli tutar: " + kdvliTutar);
+        } else
+            System.out.println("Lütfen 0'dan büyük bir tutar giriniz. !");
+    }
+}
