32cd5f9ec51f25936d4efbe5d7b707d045682596
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-02 23:08:48 +0300

    Vki Hesaplama

---
+++ src/com/company/vki.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class vki {
+
+    public static void main(String[] args) {
+        int kilo;
+        double boy, vki;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz: ");
+        boy = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kilo = input.nextInt();
+
+        vki = kilo/(boy*boy);
+        System.out.println("Vücut kitle endeksiniz: "+vki);
+    }
+}
