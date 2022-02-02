070d3ca69abd2912d9455a732388c1dccf2ada61
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-02 22:40:44 +0300

    DaireAciAlanHesaplama

---
+++ src/com/company/daireAlanHesap.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class daireAlanHesap {
+
+    public static void main(String[] args) {
+        int r, a;
+        double pi = 3.14, alan, cevre, aci;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Daire yarıçapını giriniz: ");
+        r = input.nextInt();
+        System.out.print("Daire merkez açısının ölçüsünü giriniz: ");
+        a = input.nextInt();
+
+        alan = (pi*r*r);
+        cevre = (2*pi*r);
+        aci = (pi * (r*r) * a) / 360;
+
+        System.out.println("Daire alanı: "+alan);
+        System.out.println("Daire çevresi: "+cevre);
+        System.out.println("Daire diliminin alanı: "+aci);
+    }
+}
