e0de1de1c910b980109cb9e277017a5c4d626248
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-06 21:56:16 +0300

    0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan program.

---
+++ src/com/company/ortalamaBulma.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ortalamaBulma {
+
+    public static void main(String[] args) {
+        int k;
+        int sayac = 0, toplam = 0, ortalama;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        k = input.nextInt();
+
+        for (int i = 0; i <= k; i++) {
+            if (i % 3 == 0 || i % 4 == 0) {
+                toplam += i;
+                sayac++;
+            }
+        }
+        ortalama = toplam / sayac;
+        System.out.println("0'dan klavyeden girilen sayıya kadar 3'e ve 4'e bölünenlerin toplamı : " + toplam);
+        System.out.println("0'dan klavyeden girilen sayıya kadar 3'e ve 4'e bölünenlerin ortalaması : " + ortalama);
+    }
+}
