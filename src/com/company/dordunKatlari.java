ab2d0c887e535f897704f459cdedf5d3cc388ecb
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-06 22:23:32 +0300

    Tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan program.

---
+++ src/com/company/dordunKatlari.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class dordunKatlari {
+
+    public static void main(String[] args) {
+        int total = 0, n;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("Bir sayı Giriniz: ");
+            n = scan.nextInt();
+            if (n % 4 == 0) {
+                total += n;
+            }
+        } while (n % 2 == 0);
+        System.out.println("2 ve 4 tam bölünen sayıların toplamı : " + total);
+    }
+}
