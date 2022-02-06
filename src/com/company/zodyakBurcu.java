32b49597a2e076ddd37d0c779b7bb37f66959081
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-06 16:30:57 +0300

    Doğum yılına göre Çin zodyak burcu hesaplayan program.

---
+++ src/com/company/zodyakBurcu.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class zodyakBurcu {
+
+    public static void main(String[] args) {
+        int dogumYili, zodyak;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Doğum yılınızı giriniz: ");
+        dogumYili = input.nextInt();
+
+        zodyak = dogumYili % 12;
+        switch (zodyak) {
+            case 0:
+                System.out.println("Cin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Cin Zodyak Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Cin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Cin Zodyak Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Cin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Cin Zodyak Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Cin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Cin Zodyak Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Cin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Cin Zodyak Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Cin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Cin Zodyak Burcunuz : Koyun");
+                break;
+            default:
+                System.out.println("Hatalı bir veri girdiniz.");
+        }
+    }
+}
