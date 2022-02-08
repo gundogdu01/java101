f38c8472378cb4f86dce729066d45d0f988b0156
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-08 21:05:57 +0300

    Kullanıcıdan alınan değerlere göre üs hesaplayan program.

---
+++ src/com/company/usluSayi.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class usluSayi {
+
+    public static void main(String[] args) {
+        int n, k, total = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("üssü alınacak sayıyı giriniz: ");
+        n = input.nextInt();
+        System.out.print("üs olacak sayı: ");
+        k = input.nextInt();
+
+        for (int i = 1; i<=k; i++){
+            total *= n;
+        }
+        System.out.println("Sonuç: "+total);
+    }
+}
