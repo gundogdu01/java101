f8a725c8a673b3ac87e2a11ddf081618114e31c0
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-14 22:09:02 +0300

    Fibonacci serisi hesaplayan program.

---
+++ src/com/company/fibonacci.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class fibonacci {
+
+    public static void main(String[] args) {
+        int num;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Eleman sayısını giriniz : ");
+        num = input.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        for (int i = 0; i <= num; i++) {
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+}
