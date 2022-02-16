ee914a938fc12802e95f5a232ee6c5a5413a211b
Muhammet Enes Gundogdu <enesgundogduu@outlook.com>
2022-02-16 22:15:27 +0300

    Asal sayı bulan program.

---
+++ src/com/company/recursiveAsalBulma.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class recursiveAsalBulma {
+
+    static boolean isPrime(int sayi, int a){
+        if(a>=sayi){
+            return true;
+        }
+        if(sayi%a==0){
+            return false;
+        }
+        else return isPrime(sayi,a+1);
+    }
+
+    public static void main(String[] args) {
+        int sayi;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz : ");
+        sayi = scan.nextInt();
+        System.out.println(isPrime(sayi,2)?"Asal Sayıdır":"Asal Sayı Değildir");
+    }
+}
