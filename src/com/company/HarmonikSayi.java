9ec3b681c708ffd0d5fa066724f2b04ffa992301
gundogdu01 <enesgundogduu@outlook.com>
2022-03-13 16:58:21 +0300

    Harmonik sayıları bulan program.

--- src/com/company/HarmonikSayi.java
+++ src/com/company/HarmonikSayi.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class harmonikSayi {
+public class HarmonikSayi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
