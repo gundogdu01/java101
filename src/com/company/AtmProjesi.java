7d8c2a8d1bce66bc677f1275967a3a335fe56412
gundogdu01 <enesgundogduu@outlook.com>
2022-03-13 16:43:56 +0300

    Armstrong sayılar.

--- src/com/company/AtmProjesi.java
+++ src/com/company/AtmProjesi.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class atmProjesi {
+public class AtmProjesi {
 
     public static void main(String[] args) {
         String userName, password;
