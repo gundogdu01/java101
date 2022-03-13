e451cf98e268e923bc5b9cc1ed982b9046b6a85c
gundogdu01 <enesgundogduu@outlook.com>
2022-03-13 17:08:20 +0300

    Doğum yılına göre Çin zodyak burcu hesaplayan program.

--- src/com/company/ZodyakBurcu.java
+++ src/com/company/ZodyakBurcu.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class zodyakBurcu {
+public class ZodyakBurcu {
 
     public static void main(String[] args) {
         int dogumYili, zodyak;
